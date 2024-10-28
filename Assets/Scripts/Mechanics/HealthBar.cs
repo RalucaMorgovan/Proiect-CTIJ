72d442471bac882a5a95801e6d3f5f067409cbad
RalucaMorgovan <morgovanraluca1@gmail.com>
2024-10-28 14:06:43 +0200

    Initial Commit

---
+++ Assets/Scripts/Mechanics/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Platformer.Mechanics;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalHealthBar;
+    [SerializeField] private Image currentHealthBar;
+
+    private void Start()
+    {
+        currentHealthBar.fillAmount = (float)playerHealth.CurrentHP / playerHealth.maxHP/10;
+    }
+
+    private void Update()
+    {
+        currentHealthBar.fillAmount = (float)playerHealth.CurrentHP / playerHealth.maxHP / 10;
+    }
+}
